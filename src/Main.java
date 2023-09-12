bc40feaadbcb06f4db3cb7e964f84ec2e0ca28de
brookeaye <brookey2026@student.cis.edu.hk>
2023-09-12 14:04:25 +0800

    I am light as a feather and I'm fresh as the air

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int result = add(5, 8);
+        System.out.println(result);
+
+        String resultTwo = greeting("Mr. Ewing");
+        System.out.println(resultTwo);
+
+        int resultThree = add(5, 6, 7, 8);
+        System.out.println(resultThree);
+
+        printMe("I'm light as a feather and I'm fresh as the air");
+    }
+
+    public static int add(int numOne, int numTwo){
+        return numOne + numTwo;
+    }
+
+    public static String greeting(String name){
+        return "Bonjour, " + name + "!";
+
+    }
+
+    public static int add(int numOne, int numTwo, int numThree, int numFour){
+        return add(add(numOne, numTwo), add(numThree, numFour));
+
+    }
+
+    public static void printMe(String toBePrinted){
+        System.out.println(toBePrinted);
+    }
+
 }
